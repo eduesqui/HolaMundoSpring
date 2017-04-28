cf81238b12be5755c48536f76ffe5bb9818149bf
Eduardo <eduesqui87@gmail.com>
2017-04-27 23:23:55 -0500

    [FIX] Se agrego la libreria de log4j

--- src/main/java/com/eduesqui/services/impl/EjemploServicioPremium.java
+++ src/main/java/com/eduesqui/services/impl/EjemploServicioPremium.java
@@ -8,7 +8,7 @@ public class EjemploServicioPremium implements IEjemploServicio{
 
 	@Override
 	public String saluda(String nombre) {
-		return "Buen dias Señor , " + nombre + " En que puedo servirle?";
+		return "Buen dias Sr " + nombre + " En que puedo servirle?";
 	}
 
 }
